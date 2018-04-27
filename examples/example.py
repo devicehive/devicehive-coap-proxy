f07288757321fa0256383d8038b4dc11a8e3535d
platon <platon.dimka@gmail.com>
2018-04-27 13:09:38 +0300

    Add HelperClass to override post method

--- examples/example.py
+++ examples/example.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 
 from coapthon import defines
-from coapthon.messages.request import Request
 from coapthon.messages.option import Option
-from coapthon.client.helperclient import HelperClient
+from coapthon.utils import generate_random_token
+from coapthon.client import helperclient
 import json
 import time
 import uuid
@@ -12,7 +12,7 @@ import uuid
 SERVER_HOST = '127.0.0.1'
 SERVER_PORT = 5683
 # Put your access token
-ACCESS_TOKEN = 'eyJhbGciOiJIUzI1NiJ9.eyJwYXlsb2FkIjp7ImEiOlsyLDMsNCw1LDYsNyw4LDksMTAsMTEsMTIsMTUsMTYsMTddLCJlIjoxNTI0NzQ5ODcxNDUwLCJ0IjoxLCJ1IjoyMzg4NiwibiI6WyIyMDE2MyJdLCJkdCI6WyIqIl19fQ.gGPFn8p_BXCbhmXZTPDRUZCkMmglpE8wdLmROFSCBCk'
+ACCESS_TOKEN = 'eyJhbGciOiJIUzI1NiJ9.eyJwYXlsb2FkIjp7ImEiOlsyLDMsNCw1LDYsNyw4LDksMTAsMTEsMTIsMTUsMTYsMTddLCJlIjoxNTI0ODIyNDM5NDM2LCJ0IjoxLCJ1IjoyMzg4NiwibiI6WyIyMDE2MyJdLCJkdCI6WyIqIl19fQ.X2KDSlECclGQiOq4swIC0bdQavSBP3Zu7y4qPYE6LCo'
 # Put your device id
 DEVICE_ID = 'CoAP-python-test-device'
 DEVICE_COMMAND = 'Test-command'
@@ -24,6 +24,21 @@ MESSAGE_ID_OPTION = 111
 defines.OptionRegistry.LIST[MESSAGE_ID_OPTION] = \
     defines.OptionItem(MESSAGE_ID_OPTION, "MESSAGE_ID", defines.STRING,
                        False, None)
+
+
+class HelperClient(helperclient.HelperClient):
+    """Helper client"""
+
+    def post(self, path, payload, callback=None, timeout=None, *options):
+        request = self.mk_request(defines.Codes.POST, path)
+        request.token = generate_random_token(2)
+        request.payload = payload
+        for number, value in options:
+            option = Option()
+            option.number = number
+            option.value = value
+            request.add_option(option)
+        return self.send_request(request, callback, timeout)
 
 
 class DeviceHiveCoAPClientException(Exception):
@@ -40,20 +55,21 @@ class DeviceHiveCoAPClient(object):
         self._path = path
         self._timeout = timeout
         self._client = HelperClient(server=(host, port))
-        self._event_observe_request()
+        self._event_request()
 
-    def _observe_request(self, path, *options):
-        request = Request()
-        request.destination = self._client.server
-        request.code = defines.Codes.GET.number
-        request.uri_path = path
-        request.observe = 0
-        for number, value in options:
-            option = Option()
-            option.number = number
-            option.value = value
-            request.add_option(option)
-        return request
+    def _event_request(self):
+        self._client.observe(self._path, self._event_callback, self._timeout)
+
+    def _event_callback(self, response):
+        if response.payload is None:
+            return
+        payload = self._decode_response_payload(response.payload)
+        if self._message_id is None:
+            self._message_id = payload['id']
+            return
+        self._events[payload['requestId']] = payload
+        print('---EVENT-PAYLOAD---')
+        print(payload)
 
     @staticmethod
     def _decode_response_payload(payload):
@@ -62,19 +78,6 @@ class DeviceHiveCoAPClient(object):
     @staticmethod
     def _encode_request_payload(payload):
         return json.dumps(payload)
-
-    def _event_observe_callback(self, response):
-        payload = self._decode_response_payload(response.payload)
-        if self._message_id is None:
-            self._message_id = payload['id']
-            return
-        self._events[payload['requestId']] = payload
-        print(self._events)
-
-    def _event_observe_request(self):
-        request = self._observe_request(self._path)
-        self._client.send_request(request, self._event_observe_callback,
-                                  self._timeout)
 
     def _wait_message_id(self):
         while self._message_id is None:
@@ -88,22 +91,21 @@ class DeviceHiveCoAPClient(object):
                 del self._events[request_id]
                 return event
 
-    def _message_id_observe_request(self, payload):
+    def _message_id_request(self, payload):
         self._wait_message_id()
         request_id = str(uuid.uuid4())
-        request = self._observe_request(self._path, (MESSAGE_ID_OPTION,
-                                                     self._message_id))
         payload['requestId'] = request_id
-        request.payload = self._encode_request_payload(payload)
+        payload = self._encode_request_payload(payload)
 
         def empty_callback(response):
-            print('---response---')
-            print(response)
-        self._client.send_request(request, empty_callback, self._timeout)
+            print('---UNEXPECTED-RESPONSE---')
+            print(response.payload)
+        self._client.post(self._path, payload, empty_callback, self._timeout,
+                          (MESSAGE_ID_OPTION, self._message_id))
         return request_id
 
-    def _sync_message_id_observe_request(self, payload):
-        request_id = self._message_id_observe_request(payload)
+    def _wait_message_id_request(self, payload):
+        request_id = self._message_id_request(payload)
         payload = self._wait_event(request_id)
         if payload['status'] != 'success':
             raise DeviceHiveCoAPClientException(
@@ -116,7 +118,7 @@ class DeviceHiveCoAPClient(object):
             'action': 'authenticate',
             'token': access_token,
         }
-        self._sync_message_id_observe_request(payload)
+        self._wait_message_id_request(payload)
 
     def create_device(self, device_id):
         payload = {
@@ -126,22 +128,20 @@ class DeviceHiveCoAPClient(object):
                 'name': device_id
             }
         }
-        self._sync_message_id_observe_request(payload)
+        self._wait_message_id_request(payload)
 
     def send_command(self, device_id, command_name):
         payload = {
-            'action': 'notification/insert',
+            'action': 'command/insert',
             'deviceId': device_id,
             'command': {
                 'command': command_name
             }
         }
-        payload = self._sync_message_id_observe_request(payload)
-        print(payload)
+        self._wait_message_id_request(payload)
+
 
 dh_client = DeviceHiveCoAPClient(SERVER_HOST, SERVER_PORT)
 dh_client.authorize(ACCESS_TOKEN)
 dh_client.create_device(DEVICE_ID)
-print('!!!')
 dh_client.send_command(DEVICE_ID, DEVICE_COMMAND)
-print('!!!!!!')
